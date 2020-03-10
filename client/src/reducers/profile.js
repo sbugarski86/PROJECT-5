5a125a8905aba572d9fd6e23ba1fbf02fefffff2
Stevan Bugarski <bugarski.stevan@gmail.com>
2020-03-10 17:55:10 -0400

    Displaying github repos added and profile reducers updated

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -30,7 +30,8 @@ export default function(state = initialState, action) {
       return {
         ...state,
         profiles: payload,
-        loading: false
+        loading: false,
+        profile: null
       }
     case PROFILE_ERROR:
       return {
