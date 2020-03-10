2e50acafb581593c63a5b28b229a0411761b9270
Stevan Bugarski <bugarski.stevan@gmail.com>
2020-03-10 16:09:25 -0400

    Display profiles code added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -12,9 +12,13 @@ const Navbar = props => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/profiles'>Developers</Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
-        <i className='fas fa-user'></i>{' '}
-        <span className='hide-sm'>Dashboard</span> </Link>
+          <i className='fas fa-user'></i>{' '}
+          <span className='hide-sm'>Dashboard</span>{' '}
+        </Link>
       </li>
       <li>
         <a onClick={logout} href='#!'>
@@ -27,7 +31,7 @@ const Navbar = props => {
   const guestLinks = (
     <ul>
       <li>
-        <a href='#!'>Developers</a>
+        <Link to='/profiles'>Developers</Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
